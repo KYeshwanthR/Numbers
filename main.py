84bd7d17312f9d51bc88fb5ce5b1e76a127f7b03
YeshwanthRam <yeswanthram.kamavarapu@gmail.com>
2023-12-29 02:38:40 +0530

    Main Refactor
    Moved Subs data from .py file to data.json. data is being read using json library.
    Input through st.data_editor is replaced with st.text_input using re the input will be validated to make sure the input is in space of allowed grades.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import re, json
 
 st.set_page_config(layout='wide',page_title='Numbers')
 
@@ -9,89 +10,18 @@ if st.button("Result Links"):
 st.header("Initally Grades are set to F")
 st.header("Change them to your awarded Grades by clicking on Grade cells in each semeter Table")
 
-def title():
-    st.markdown(f"<h1 style='text-align: center; color: red;'>Do not Select Emtpy option as Grade</h1>", unsafe_allow_html=True)
-
-
-#title()
-
 points = {"A+": 10,"A": 9,"B": 8,"C": 7,"D": 6,"E": 5,"F": 0}
 
+with open('data.json','r') as d:
+    Subs = json.load(d)
 
-Subs = {
-    "Sem1" : {
-        "Technical English" : 3,
-        "Engineering Mathematics-I": 3,
-        "Basic Electrical Engineering": 3,
-        "Programming for Problem Solving": 3,
-        "Computer Aided Engineering Graphics": 3,
-        "English Communication Skills Lab" : 1.5,
-        "Basic Electrical Engineering Lab" : 1.5,
-        "Programming for Problem Solving Lab" : 1.5,
-        "Environmental Science" : 0,
-    },
-    "Sem2" : {
-        "Engineering Mathematics - II" : 3,
-        "Engineering Physics" : 3,
-        "Engineering Chemistry" : 3,
-        "Python Programming" : 3,
-        "Data Structures" : 3,
-        "Engineering Physics Lab" : 1.5,
-        "Engineering Chemistry Lab" : 1.5,
-        "Data Structures Lab" : 1.5,
-        "Constitution of India, Professional Ethics & Human Right" : 0,
-    },
 
-    "Sem3" : {
-        "Probability Distributions & Statistical Methods" : 3,
-        "Data Science Using Python" : 2,
-        "Data Base Management Systems Lab" : 1.5,
-        "Java Programming Lab": 1.5,
-        "Analog & Digital Electronics Lab" : 1.5,
-        "Data Base Management Systems" : 3.0,
-        "Java Programming" : 3.0,
-        "Computer Organization": 3.0,
-        "Analog & Digital Electronics" : 3.0,
-        "Biology for Engineers" : 0,
-    },
-
-    "Sem4" : {
-        "Discrete Mathematics" : 3,
-        "Engineering Economics and Financial Management" : 3,
-        "Operating systems" : 3,
-        "Design and Analysis of Algorithms" : 3,
-        "Software Engineering" : 3,
-        "Operating Systems Lab" : 1.5,
-        "Design and Analysis of Algorithms Lab" : 1.5,
-        "Software Engineering Lab" : 1.5,
-        "MEAN stack Technologies" : 2,	
-    },
-
-    "Sem5" : {
-        "ATCD" : 3,
-        "Summer Internship" : 1.5,
-        "SSA" : 2.0,
-        "DWM Lab" : 1.5,
-        "CN Lab" : 1.5,
-        "FIC" : 3,
-        "SPM" : 3,
-        "DWM" : 3,
-        "CN" : 3,
-        "IPR" : 0,
-    },
-}
-
-def makedf(sem):
+def makedf(subs,creds,grads):
     data = {
-        "Subjects" : [],
-        "Grade" : [],
-        "Credits" : [],
+        "Subjects" : subs,
+        "Grade" : grads,
+        "Credits" : creds,
     }
-
-    for sub , cred in Subs[sem].items():
-        data["Subjects"].append(sub)
-        data["Grade"].append("F")
-        data["Credits"].append(round(cred,2))
 
     return pd.DataFrame(data)
 
@@ -106,87 +36,38 @@ def calcPointAvg(df: pd.DataFrame):
 
     return round(sgp/sum(ACredits),2)
 
+def is_valid_input(input_string):
+    # Regular expression pattern with case-insensitivity
+    pattern = r'^(a\+|[abcdef])+$'
+    return bool(re.match(pattern, input_string, re.IGNORECASE))
 
-c = st.columns(2)
+def convert_to_list(input_string):
+    # Regular expression to find 'A+' and individual characters
+    pattern = r'a\+|[abcdef]'
+    return re.findall(pattern, input_string, re.IGNORECASE)
 
-gradescol = {
-    "Subjects" : "Subjects",
-    "Credits" : "Credits",
-        "Grade" : st.column_config.SelectboxColumn(
-            help = "Click on your Grade from DropDown",
-            options = list(points.keys()),
-            required=True,
-        ),
-    }
+st.write(len(Subs))
 
-dcols = ['Subjects','Credits']
+semresults = []
+for n in Subs:
+    allowed_grades = ['A+', 'A', 'B', 'C', 'D', 'E', 'F']
+    default = 'F'
 
-sgpa = []
-with c[0]:
-    st.write("1-1 Marks Table")
-    S1 = st.data_editor(
-        makedf("Sem1"),
-        use_container_width = True,
-        hide_index=True,
-        column_config = gradescol,
-        disabled=dcols,
-    )
-    s1r = calcPointAvg(S1)
-    if calcPointAvg(S1) > 0:
-        sgpa.append(s1r)
-    st.write(f"Sem 1 SGPA: :green[{calcPointAvg(S1)}]")
+    st.divider()
+    cols = st.columns(2)
+    gradesinput = st.text_input("Enter Grades",max_chars=len(Subs[n])*2,help=f"{allowed_grades}",key=n)
+    st.info("Enter Grades in order of Subjects")
+    if is_valid_input(gradesinput):
+        gradesinput = [n.upper() for n in convert_to_list(gradesinput)]
+    else:
+        st.error(f"allowed grades are {allowed_grades}")
+    res = makedf(list(Subs[n].keys()),list(Subs[n].values()),list(gradesinput) + ['F']*(len(Subs[n]) - len(gradesinput)))
+    st.dataframe(res,use_container_width=True,hide_index=True)
+    semresults.append(res)
 
-    st.write("1-2 Marks Table")
-    S2 = st.data_editor(
-        makedf("Sem2"),
-        use_container_width = True,
-        hide_index=True,
-        column_config = gradescol,
-        disabled=dcols,
-    )
-    s2r = calcPointAvg(S2)
-    if s2r > 0:
-        sgpa.append(s2r)
-    st.write(f"Sem 2 SGPA: :green[{calcPointAvg(S2)}]")
+    st.success(f"Sem-{n[-1]} SGPA: green [{calcPointAvg(res)}]" )
 
-with c[1]:
-    st.write("2-1 Marks Table")
-    S3 = st.data_editor(
-        makedf("Sem3"),
-        use_container_width = True,
-        hide_index=True,
-        column_config = gradescol,
-        disabled = dcols,
-    )
-    s3r = calcPointAvg(S3)
-    if s3r > 0:
-        sgpa.append(s3r)
-
-    st.write(f"Sem 3 SGPA: :green[{calcPointAvg(S3)}]")
-
-    st.write("2-2 Marks Table")
-    S4 = st.data_editor(
-        makedf("Sem4"),
-        use_container_width = True,
-        hide_index=True,
-        column_config = gradescol,
-        disabled=dcols,
-    )
-    s4r = calcPointAvg(S4)
-    if s4r > 0:
-        sgpa.append(s4r)
-    st.write(f"Sem 4 SGPA: :green[{calcPointAvg(S4)}]")
-
-S5 = st.data_editor(
-        makedf("Sem5"),
-        use_container_width = True,
-        hide_index=True,
-        column_config = gradescol,
-        disabled=dcols,
-    )
-st.write(f"Sem 5 SGPA: :green[{calcPointAvg(S5)}]")
-
-result_df = pd.concat([S1, S2, S3, S4, S5], axis=0)
+result_df = pd.concat(semresults, axis=0)
 
 st.write(f"Total Credits: {sum(tc)}")
 st.toast(f"CGPA : :green[{calcPointAvg(result_df)}]")
