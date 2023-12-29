19ca52e3a1b126a52390b5a8a67305f27510c3b0
YeshwanthRam <yeswanthram.kamavarapu@gmail.com>
2023-12-29 13:38:41 +0530

    Changed messages

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with st.expander("Result Links"):
     st.info("Links of results [1-1](http://sasi.ac.in/11apr2022/) [1-2](http://sasi.ac.in/btech12sep2022/) [2-1](http://www.sasi.ac.in/btech21regjan2023/) [2-2](https://sasi.ac.in/btech22regsupjuly2023/) [3-1](https://sasi.ac.in/btech31regnov2023/)" )
 
 st.header("Initally Grades are set to F")
-st.header("Change them to your awarded Grades by clicking on Grade cells in each semeter Table")
+st.header("Enter Grades for each sem in relative order of subjects without any spaces")
 
 points = {"A+": 10,"A": 9,"B": 8,"C": 7,"D": 6,"E": 5,"F": 0}
 
@@ -65,7 +65,7 @@ for n in Subs:
     st.dataframe(res,use_container_width=True,hide_index=True)
     semresults.append(res)
 
-    st.success(f"Sem-{n[-1]} SGPA: green [{calcPointAvg(res)}]" )
+    st.success(f"Sem-{n[-1]} SGPA: :green[{calcPointAvg(res)}]" )
 
 result_df = pd.concat(semresults, axis=0)
 
